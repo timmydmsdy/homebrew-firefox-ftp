5a5747e62eeba33879eef21ee8d8f0cf800c176b
timmydoomsday <50457679+timmydmsdy@users.noreply.github.com>
2024-10-22 19:30:39 +0000

    Create firefox.rb

---
+++ Casks/firefox.rb
@@ -0,0 +1,264 @@
+cask "firefox" do
+  version "131.0.3"
+
+  language "af" do
+    sha256 "01de08b317972089c8a3e999f35f74bb3771d6d63c2eddeca4d7e8fff509cd23"
+    "af"
+  end
+  language "ar" do
+    sha256 "0e78a9972fc6ef2b7f7a094fb17d2d91baab069f7b62ecca08acc3a69c61efbc"
+    "ar"
+  end
+  language "be" do
+    sha256 "2b1fc96fc5aaff385c28c204fa4f3576fed5091ebfcff36d41be9cb60a96dd9f"
+    "be"
+  end
+  language "bg" do
+    sha256 "a06731ab44075ff764051048272bb68f1b8ce48afdfc20a07cb99064e59c2f92"
+    "bg"
+  end
+  language "bn" do
+    sha256 "2fb45aef8721a3b0fcd80f828fd7590cfb960ad6540185c7e1e0f0395eae1070"
+    "bn"
+  end
+  language "ca" do
+    sha256 "1c0c7c36d54bf89c985bfbfb53ae82ba0793f5df3d135a7fc60d8f22a4e9c0b2"
+    "ca"
+  end
+  language "cs" do
+    sha256 "12f227a8e0398351ab14543fd54a81c4363abb11f5cc376af74e1ff3b1db41e8"
+    "cs"
+  end
+  language "de" do
+    sha256 "fff8ef8ce211567602a5db4927cd0d24239d43153943e23f725a6f53637c573d"
+    "de"
+  end
+  language "en-CA" do
+    sha256 "d8ed87a5e2fdda99218117214e3765ca252ab91cf25fa04d8472a785097029a0"
+    "en-CA"
+  end
+  language "en-GB" do
+    sha256 "0fa521424ce1dc944663f318018f76a4ec522a94bbf02dc5305ff8a36c233236"
+    "en-GB"
+  end
+  language "en", default: true do
+    sha256 "7a4714e20b4f2099056e4c79921291256278174273d57bff09771f98774ff513"
+    "en-US"
+  end
+  language "eo" do
+    sha256 "ace42e9e32c8496bbfa1115b024a08a0efca15c18fe28fe1a94e67207632c047"
+    "eo"
+  end
+  language "es-AR" do
+    sha256 "08157149b44a596621362336faee14eb05b3c6949374658e2d8dcc8ad190d149"
+    "es-AR"
+  end
+  language "es-CL" do
+    sha256 "5d6502ef65d07d58515066c1666f575589f14a0ef5c63577a5b7917db7b5d534"
+    "es-CL"
+  end
+  language "es-ES" do
+    sha256 "a784fc7dace39b3e8be8f267c5a973e041116513855e155ae9917248fb253435"
+    "es-ES"
+  end
+  language "fa" do
+    sha256 "bf66ae2b9cc0114e1b6be11dd9bedf66cae60d7eda48619acf2fa30b7a123a91"
+    "fa"
+  end
+  language "ff" do
+    sha256 "033ade624724f344a4db453af23187361766e2251ba2bb7625aa29b5fe2ddf60"
+    "ff"
+  end
+  language "fi" do
+    sha256 "27a562bb16474797d9cc45b039a0101581308fa267898996568ce512d55a7843"
+    "fi"
+  end
+  language "fr" do
+    sha256 "57c77f2034fd1f49b615b940740b8ee72f2dc05bb186a2db3b73b3e6e3684e9c"
+    "fr"
+  end
+  language "gl" do
+    sha256 "521df8bd9d0de7db74239219b2a82c2cd586354b8bc459bf286d0e4af00bfd39"
+    "gl"
+  end
+  language "gn" do
+    sha256 "f4ab4768fcb5820aaa975b1628f9525021104cda1427e015e86fe7dbac941b51"
+    "gn"
+  end
+  language "gu" do
+    sha256 "0c8c77315bf87fb2c259f8137702dda39b839b6009d79ecf7e3c2deb649f643a"
+    "gu-IN"
+  end
+  language "he" do
+    sha256 "a747550c331a5eafee828a2856940d4cfa014b3b9c3b7262dddb53a716bc3aaa"
+    "he"
+  end
+  language "hi" do
+    sha256 "b8c7efc28e483acdf8556b96171903a9f238c29c563d4345a07e63930acbe0cc"
+    "hi-IN"
+  end
+  language "in" do
+    sha256 "b8c7efc28e483acdf8556b96171903a9f238c29c563d4345a07e63930acbe0cc"
+    "hi-IN"
+  end
+  language "it" do
+    sha256 "da462b2bebcf9b7cc6880aa24dab7648da38fc69608cbf9003b8446f09073ee7"
+    "it"
+  end
+  language "ja" do
+    sha256 "a7410db66508dd927cc5ffd23748c8c9e7463e596734d401a76666295f56140a"
+    "ja-JP-mac"
+  end
+  language "ka" do
+    sha256 "a1520b4c3210442f64102e9c502e175d976c829287aa877861c7a8d937358896"
+    "ka"
+  end
+  language "ko" do
+    sha256 "ce2419f8166fe3c2338fd982220dc2674daa094ab032a525f1bafe2357eb882d"
+    "ko"
+  end
+  language "mr" do
+    sha256 "c02beb22758ef5ff0069175a6270b45c1f4fd0932a1dbd924a47f75352d2cef8"
+    "mr"
+  end
+  language "my" do
+    sha256 "3e0f2f2c6af2f6f186829eccd43bfd57e00ec0c42225d75ee2daaa72993a7f59"
+    "my"
+  end
+  language "ne" do
+    sha256 "39d37f801a4b306c69a7d7371d6263c758e677e824c3e3f42fb1d2f982f03057"
+    "ne-NP"
+  end
+  language "nl" do
+    sha256 "b56b37fbe2b0a335d1063e31b3fe9b3970e84afb116f65d63abc2f25006bed5a"
+    "nl"
+  end
+  language "pa-IN" do
+    sha256 "360a5420720ca7c5cf1cad8546a8a5a72383f158974cceba4f3f8ad796cb5ed8"
+    "pa-IN"
+  end
+  language "pl" do
+    sha256 "0575ec1afcc49999659d692c77746d2c6e95bc0cf9a8ba8f5dc5048647976a31"
+    "pl"
+  end
+  language "pt-BR" do
+    sha256 "d1471557654c791f43cc1cdacaafcfea30b70e0ea09c7b491bbccd0b7cf2a3fd"
+    "pt-BR"
+  end
+  language "pt" do
+    sha256 "a50d01b27a2c012de4c8fdfbc4ac3b3e3fc30e86cae0b322c15a286ee5eb021a"
+    "pt-PT"
+  end
+  language "ru" do
+    sha256 "b2a3ffd4dc8c30fa97bec40051ac2327c4a16bf06bbcfa02b46e063be17b957f"
+    "ru"
+  end
+  language "si" do
+    sha256 "358c748d0fe5028555a512e76de5a74c021ed0dff42e3b42487d0e22e4e6fd3c"
+    "si"
+  end
+  language "sq" do
+    sha256 "576cea8bf4da9b4bf0822b3451544d51ed6ee5294c0be0674ff26091036d2b7a"
+    "sq"
+  end
+  language "sr" do
+    sha256 "d3eda880ae565ee2d94f33d037d8fd5244ec33537c439737ca9937119b30f8cc"
+    "sr"
+  end
+  language "sv" do
+    sha256 "e5292097a0e9b11df629b5728642d293b18b1ff7b9384aecb554f90ba70f5e85"
+    "sv-SE"
+  end
+  language "ta" do
+    sha256 "ded69d06da38b89ddba64152005fe952df6095aa36965464a1ec113a0d49ef24"
+    "ta"
+  end
+  language "te" do
+    sha256 "bae2243009e6f2f153a6941acb2ba37dca01b3edca5c01e01394fb90fc8ad598"
+    "te"
+  end
+  language "th" do
+    sha256 "5f372d64a30b9d2bf49d861d3c92c78907820404cf92d4c69fc6eea93a2ffc01"
+    "th"
+  end
+  language "tl" do
+    sha256 "43d4dd62a50f0e645bc6563b1f6f2f214a7db02c9a6a69f6cf95534922276312"
+    "tl"
+  end
+  language "tr" do
+    sha256 "195cf16485a186af784907fd686db5c42223b8e187398d114dfc95e2aae46f47"
+    "tr"
+  end
+  language "uk" do
+    sha256 "5032142ac89e23896a86e8e8cea7a974447ff71d4ce6f5ff08994dd9fe7bb991"
+    "uk"
+  end
+  language "ur" do
+    sha256 "407029e4312ce0476ea5174b05b0a80189d12c20f2e5f27eeb8ddfb4c83c37fd"
+    "ur"
+  end
+  language "zh-TW" do
+    sha256 "277d9d84518848ffb2ccf7b10daf1b149f117fa8122bf527cd7a2ce82e690127"
+    "zh-TW"
+  end
+  language "zh" do
+    sha256 "e30e273d3fc6b5bc7aafadb18eb66afa88ea466229a3f7f97d2294b2a7deecf9"
+    "zh-CN"
+  end
+
+  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/#{language}/Firefox%20#{version}.dmg",
+      verified: "ftp.mozilla.org/pub/firefox/releases/"
+  name "Mozilla Firefox"
+  desc "Web browser"
+  homepage "https://www.mozilla.org/firefox/"
+
+  livecheck do
+    url "https://product-details.mozilla.org/1.0/firefox_versions.json"
+    strategy :json do |json|
+      json["LATEST_FIREFOX_VERSION"]
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: [
+    "firefox@beta",
+    "firefox@cn",
+    "firefox@esr",
+  ]
+  depends_on macos: ">= :catalina"
+
+  app "Firefox.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/firefox.wrapper.sh"
+  binary shimscript, target: "firefox"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/bash
+      exec '#{appdir}/Firefox.app/Contents/MacOS/firefox' "$@"
+    EOS
+  end
+
+  uninstall quit: "org.mozilla.firefox"
+
+  zap trash: [
+        "/Library/Logs/DiagnosticReports/firefox_*",
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.firefox.sfl*",
+        "~/Library/Application Support/CrashReporter/firefox_*",
+        "~/Library/Application Support/Firefox",
+        "~/Library/Caches/Firefox",
+        "~/Library/Caches/Mozilla/updates/Applications/Firefox",
+        "~/Library/Caches/org.mozilla.crashreporter",
+        "~/Library/Caches/org.mozilla.firefox",
+        "~/Library/Preferences/org.mozilla.crashreporter.plist",
+        "~/Library/Preferences/org.mozilla.firefox.plist",
+        "~/Library/Saved Application State/org.mozilla.firefox.savedState",
+        "~/Library/WebKit/org.mozilla.firefox",
+      ],
+      rmdir: [
+        "~/Library/Application Support/Mozilla", #  May also contain non-Firefox data
+        "~/Library/Caches/Mozilla",
+        "~/Library/Caches/Mozilla/updates",
+        "~/Library/Caches/Mozilla/updates/Applications",
+      ]
+end
